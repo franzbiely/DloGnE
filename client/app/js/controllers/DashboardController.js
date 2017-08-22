2981d03ea5edacbb57b40a51754d2590c61a2226
Francis Albores <mrthemetribe@gmail.com>
2017-08-23 04:27:16 +0800

    Temporarily disabled dash home

--- client/app/js/controllers/DashboardController.js
+++ client/app/js/controllers/DashboardController.js
@@ -1,11 +1,17 @@
-angular.module('MetronicApp').controller('DashboardController', function($rootScope, $scope, $http, $timeout) {
-    $scope.$on('$viewContentLoaded', function() {   
-        // initialize core components
-        App.initAjax();
-    });
+angular.module('MetronicApp').controller('DashboardController', 
+	function($state, $rootScope, $scope, $http, $timeout) {
+	// $scope.$on('$viewContentLoaded', function() {   
+ //        // initialize core components
+ //        App.initAjax();
+ //    });
+	/*
+    
 
     // set sidebar closed and body solid layout mode
     $rootScope.settings.layout.pageContentWhite = true;
     $rootScope.settings.layout.pageBodySolid = false;
     $rootScope.settings.layout.pageSidebarClosed = false;
+    */
+    // alert('test');
+    $state.go('property.list');
 });
