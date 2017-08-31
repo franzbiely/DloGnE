76392f5d33e603ba9ddb58c31fd585a90b0778a8
Francis Albores <mrthemetribe@gmail.com>
2017-08-31 09:32:24 +0800

    Property-use Angular

--- client/app/js/controllers/UseController.js
+++ client/app/js/controllers/UseController.js
@@ -1,6 +1,6 @@
 /* Setup blank page controller */
-angular.module('MetronicApp').controller('UseController', ['$rootScope', '$scope', 'settings', function($rootScope, $scope, settings) {
-    $scope.$on('$viewContentLoaded', function() {   
+angular.module('MetronicApp').controller('UseController', ['$rootScope', '$scope', 'settings', '$http', '$compile', function($rootScope, $scope, settings, $http, $compile) {
+    $scope.$on('$viewContentLoaded', function() {
         // initialize core components
         App.initAjax();
 
@@ -9,26 +9,105 @@ angular.module('MetronicApp').controller('UseController', ['$rootScope', '$scope
         // $rootScope.settings.layout.pageBodySolid = false;
         // $rootScope.settings.layout.pageSidebarClosed = false;
     });
-    $scope.useModal = function() {
-    	var form = $('<form id="frmCity" role="form" class="form-horizontal">\
+    
+    
+    $scope.foo = function() {
+        console.log('test');
+    }
+    $scope.useModal = function(key = -1) {
+        console.log($scope.property_uses[key].name);
+
+        var form = '<form ng-submit="foo()" id="frmUse" name="frmUse" role="form" class="form-horizontal">\
                         <div class="form-body">\
                             <div class="form-group">\
                                 <label class="col-md-4 control-label">Use Name</label>\
                                 <div class="col-md-8">\
                                     <div class="input-icon right">\
-                                        <i class="fa fa-info-circle tooltips" data-original-title="Email address" data-container="body"></i>\
-                                        <input type="text" class="form-control"> </div>\
-                                </div>\
+                                        <i class="fa fa-info-circle tooltips" data-container="body"></i>';
+            if(key > -1) {
+                form +=                     '<input type="text" value="'+$scope.property_uses[key].name+'" class="form-control" name="use" id="use"> </div>';
+            }
+            else {
+                form +=                     '<input type="text" class="form-control" name="use" id="use"> </div>';    
+            }
+            form +=             '</div>\
                             </div>\
                         </div>\
-                    </form>');
-    	bootbox.confirm({
-    		title : "Add New Use",
-    		message : form,
-    		callback : function(res) {
-	    		if(res)
-	    			$('#frmCity').submit();
-	    	}
-    	});
+                    </form>';
+            form = $(form);
+        bootbox.confirm({
+            title: "Add New Use",
+            message: form,
+            callback: function(res) {
+                if (res){
+                    $scope.property_use = $('#frmUse')[0]['elements'].use.value;
+                    $scope.$apply();
+                    if(key > -1) {
+                        // Edit
+                        $scope.property_uses[key].name = $scope.property_use;
+                        $scope.$apply();
+                        $scope.updateUse($scope.property_uses[key].id);
+                    }
+                    else {
+                        // New
+                        $scope.addUse();
+                    }
+
+                }
+            }
+        });
     }
+
+    $scope.property_uses = [];
+    $scope.error;
+    $scope.property_use;
+
+    // Display
+    $scope.init = function() {
+        $http.get('http://localhost:8000/api/v1/property_use').success(function(property_uses) {
+            $scope.property_uses = property_uses.data;
+            console.log($scope.property_uses);
+        }).error(function(error) {
+            $scope.error = error;
+        })
+    };
+    $scope.init();
+
+    // Delete
+    $scope.deleteUse = function(index, useId) {
+        console.log(index, useId);
+
+        $http.delete('http://localhost:8000/api/v1/property_use/' + useId)
+            .success(function() {
+                $scope.property_uses.splice(index, 1);
+            });;
+    }
+
+    // Add
+    $scope.addUse = function() {
+ 
+        $http.post('http://localhost:8000/api/v1/property_use', {
+            name: $scope.property_use
+        }).success(function(response) {
+
+            $scope.property_uses.push(response.data);
+            console.log($scope.property_use);
+            $scope.property_use = '';
+
+        }).error(function(){
+            console.log("error");
+        });
+    };
+
+    // Update
+    $scope.updateUse = function(useId){
+      $http.put('http://localhost:8000/api/v1/property_use/' + useId, {
+            name: $scope.property_use
+        }).success(function(response) {
+            console.log("Updated Successfully");
+        }).error(function(){
+            console.log("error");
+        });
+    }
+
 }]);
