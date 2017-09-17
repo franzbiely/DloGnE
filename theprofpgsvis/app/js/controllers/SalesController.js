8397f8017820182e232794ca1fdfa499fe4d2116
Francis Albores <mrthemetribe@gmail.com>
2017-09-17 15:06:41 +0800

    Sales front end

--- theprofpgsvis/app/js/controllers/SalesController.js
+++ theprofpgsvis/app/js/controllers/SalesController.js
@@ -2,39 +2,42 @@ angular.module('MetronicApp').controller('SalesController', function($rootScope,
     $scope.$on('$viewContentLoaded', function() {   
         App.initAjax(); // initialize core components        
     });
-
-    // set sidebar closed and body solid layout mode
-    // $rootScope.settings.layout.pageContentWhite = true;
-    // $rootScope.settings.layout.pageBodySolid = true;
-    // $rootScope.settings.layout.pageSidebarClosed = false;
-
     $scope.showResult = function() {
     	$scope.resultReady = true;
     }
-
     $scope.hasActions = $scope.$parent.type !== "reports" ? true : false;
 
 
     // Dropzone
-    Dropzone.autoDiscover = false;
-    $scope.dzOptions = {
-        url : '/alt_upload_url',
-        paramName : 'photo',
-        maxFilesize : '10',
-        acceptedFiles : 'image/jpeg, images/jpg, image/png',
-        addRemoveLinks : true
+    // Dropzone.autoDiscover = false;
+    // $scope.dzOptions = {
+    //     url : '/alt_upload_url',
+    //     paramName : 'photo',
+    //     maxFilesize : '10',
+    //     acceptedFiles : 'image/jpeg, images/jpg, image/png',
+    //     addRemoveLinks : true
+    // };
+    // $scope.dzCallbacks = {
+    //     'addedfile' : function(file){
+    //         console.log(file);
+    //         $scope.newFile = file;
+    //     },
+    //     'success' : function(file, xhr){
+    //         console.log(file, xhr);
+    //     }
+    // };
+    // $scope.dzMethods = {};
+    // $scope.removeNewFile = function(){
+    //     $scope.dzMethods.removeFile($scope.newFile); //We got $scope.newFile from 'addedfile' event callback
+    // }
+
+    // Display
+    $scope.init = function() {
+        $http.get($rootScope.apiURL + 'v1/sale').success(function(res) {
+            $scope.sales = res.data;
+        }).error(function(error) {
+            console.log('Service error : ',error);
+        })
     };
-    $scope.dzCallbacks = {
-        'addedfile' : function(file){
-            console.log(file);
-            $scope.newFile = file;
-        },
-        'success' : function(file, xhr){
-            console.log(file, xhr);
-        }
-    };
-    $scope.dzMethods = {};
-    $scope.removeNewFile = function(){
-        $scope.dzMethods.removeFile($scope.newFile); //We got $scope.newFile from 'addedfile' event callback
-    }
+    $scope.init();
 });
