7247212251c791bac31e6dd083b151d8c1dec4cc
Francis Albores <mrthemetribe@gmail.com>
2017-09-23 13:59:03 +0800

    Property Delete

--- theprofpgsvis/app/js/controllers/PropertiesController.js
+++ theprofpgsvis/app/js/controllers/PropertiesController.js
@@ -2,6 +2,10 @@ angular.module('MetronicApp').controller('PropertiesController',
     function($rootScope, $scope, $http, settings) {
         $scope.$on('$viewContentLoaded', function() {   
             App.initAjax();
+            $scope.properties = [];
+            $scope.error;
+            $scope.property;
+            $scope.isDisabled = false;
         });
         $scope.hasActions = $scope.$parent.type !== "reports" ? true : false;
         // Display
@@ -13,6 +17,16 @@ angular.module('MetronicApp').controller('PropertiesController',
             })
         };
         $scope.init();
+
+        // Delete
+        $scope.deleteProperty = function(index, id) {
+            $scope.isDisabled = true;
+            $http.delete($rootScope.apiURL + 'v1/property/' + id + '?token='+localStorage.getItem('satellizer_token'))
+                .success(function() {
+                    $scope.properties.splice(index, 1);
+                    $scope.isDisabled = false;
+                });;
+        }
 
     }
 );
