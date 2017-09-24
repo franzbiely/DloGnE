a6e221e915fc94a6d6dc5d06fb88c7daa0d4b2be
Francis Albores <mrthemetribe@gmail.com>
2017-09-24 21:04:25 +0800

    User CRUD and Disable

--- theprofpgsvis/app/js/controllers/UsersController.js
+++ theprofpgsvis/app/js/controllers/UsersController.js
@@ -1,40 +1,96 @@
 /* Setup blank page controller */
-angular.module('MetronicApp').controller('UsersController', function($rootScope, $scope, settings, $http, $rootScope) {
+angular.module('MetronicApp').controller('UsersController', function($rootScope, $scope, settings, $http, $compile, $rootScope) {
     $scope.$on('$viewContentLoaded', function() {   
         App.initAjax();
         $scope.users = [];
         $scope.user;
     });
+    $scope.user_role_selected;
+    $scope.user_role_options = [
+        { id : 'valuer', label : 'Valuer' },
+        { id : 'data-entry', label : 'Data Entry'}
+    ];
 
-    $scope.classModal = function(key = -1) {
-        var form = '<form ng-submit="foo()" id="frmUser" name="frmUser" role="form" class="form-horizontal">\
+    $scope.showModal = function(key = -1) {
+        if(key > -1) {
+            for(i = 0; i < $scope.user_role_options.length; i++){
+                if($scope.user_role_options[i].id == $scope.users[key].role) 
+                    $scope.user_role_selected = $scope.user_role_options[i];
+            }
+            
+        }
+
+        var form = '<form id="frmUser" role="form" class="form-horizontal" ng-controller="UsersController">\
                         <div class="form-body">\
                             <div class="form-group">\
-                                <label class="col-md-4 control-label">Class Name</label>\
+                                <label class="col-md-4 control-label">Username</label>\
+                                <div class="col-md-8">\
+                                    <div class="input-icon right">\
+                                        <i class="fa fa-info-circle tooltips" data-container="body"></i>';
+                            
+            if(key > -1) {
+                form +=                     '<input type="text" value="'+$scope.users[key].username+'" class="form-control" name="username" id="username"> </div>';
+            }
+            else {
+                form +=                     '<input type="text" class="form-control" name="username" id="username"> </div>';    
+            }
+            form +=             '</div>\
+                            </div>\
+                            <div class="form-group">\
+                                <label class="col-md-4 control-label">Full Name</label>\
                                 <div class="col-md-8">\
                                     <div class="input-icon right">\
                                         <i class="fa fa-info-circle tooltips" data-container="body"></i>';
             if(key > -1) {
-                form +=                     '<input type="text" value="'+$scope.users[key].name+'" class="form-control" name="class" id="class"> </div>';
+                form +=                     '<input type="text" value="'+$scope.users[key].name+'" class="form-control" name="name" id="name"> </div>';
             }
             else {
-                form +=                     '<input type="text" class="form-control" name="class" id="class"> </div>';    
+                form +=                     '<input type="text" class="form-control" name="name" id="name"> </div>';    
             }
             form +=             '</div>\
                             </div>\
+                            <div class="form-group">\
+                                <label class="col-md-4 control-label">Email</label>\
+                                <div class="col-md-8">\
+                                    <div class="input-icon right">\
+                                        <i class="fa fa-info-circle tooltips" data-container="body"></i>';
+            if(key > -1) {
+                form +=                     '<input type="email" value="'+$scope.users[key].email+'" class="form-control" name="email" id="email"> </div>';
+            }
+            else {
+                form +=                     '<input type="text" class="form-control" name="email" id="email"> </div>';    
+            }
+            form +=             '</div>\
+                            </div>\
+                            <div class="form-group">\
+                                <label class="col-md-4 control-label">Role</label>\
+                                <div class="col-md-8">\
+                                    <div class="input-icon right">\
+                                        <i class="fa fa-info-circle tooltips" data-container="body"></i>\
+                                        <select id="role" name="role" ng-model="user_role_selected" ng-options="item as item.label for item in user_role_options track by item.id" class="form-control"></select>\
+                                    </div>\
+                                </div>\
+                            </div>\
                         </div>\
                     </form>';
-            form = $(form);
+            form = $compile(form)($scope);
         bootbox.confirm({
             title: "Add New Class",
+            scope : $scope,
             message: form,
             callback: function(res) {
                 if (res){
-                    $scope.user = $('#frmUser')[0]['elements'].class.value;
+                    $scope.user.username = $('#frmUser')[0]['elements'].username.value;
+                    $scope.user.name = $('#frmUser')[0]['elements'].name.value;
+                    $scope.user.email = $('#frmUser')[0]['elements'].email.value;
+                    $scope.user.role = $('#frmUser')[0]['elements'].role.options[ $('#frmUser')[0]['elements'].role.selectedIndex ].value;
                     $scope.$apply();
                     if(key > -1) {
                         // Edit
-                        $scope.users[key].name = $scope.user;
+                        $scope.users[key].username = $scope.user.username;
+                        $scope.users[key].name = $scope.user.name;
+                        $scope.users[key].email = $scope.user.email;
+                        $scope.users[key].role = $scope.user.role;
                         $scope.$apply();
                         $scope.updateUser($scope.users[key].id);
                     }
@@ -62,7 +118,7 @@ angular.module('MetronicApp').controller('UsersController', function($rootScope,
     $scope.init();
 
     // Delete
-    $scope.deleteUser = function(index, id) {
+    $scope.delete = function(index, id) {
         console.log(index, id);
 
         $http.delete($rootScope.apiURL + 'v1/users/' + id + '?token='+localStorage.getItem('satellizer_token'))
@@ -75,7 +131,10 @@ angular.module('MetronicApp').controller('UsersController', function($rootScope,
     $scope.addUser = function() {
  
         $http.post($rootScope.apiURL + 'v1/users?token='+localStorage.getItem('satellizer_token'), {
-            name: $scope.user,
+            username: $scope.user.username,
+            name: $scope.user.name,
+            email: $scope.user.email,
+            role: $scope.user.role
         }).success(function(response) {
 
             $scope.users.push(response.data);
@@ -90,7 +149,10 @@ angular.module('MetronicApp').controller('UsersController', function($rootScope,
     // Update
     $scope.updateUser = function(id){
       $http.put($rootScope.apiURL + 'v1/users/' + id + '?token='+localStorage.getItem('satellizer_token'), {
-            name: $scope.user
+            username: $scope.user.username,
+            name: $scope.user.name,
+            email: $scope.user.email,
+            role: $scope.user.role
         }).success(function(response) {
             console.log("Updated Successfully");
         }).error(function(){
@@ -98,7 +160,13 @@ angular.module('MetronicApp').controller('UsersController', function($rootScope,
         });
     }
 
-    $scope.logout = function() {
-        
+    $scope.setDisable = function(id){
+      $http.patch($rootScope.apiURL + 'v1/users/' + id + '?token='+localStorage.getItem('satellizer_token'), {
+            isDisabled : 1
+        }).success(function(response) {
+            console.log("Updated Successfully");
+        }).error(function(){
+            console.log("error");
+        });
     }
 });
