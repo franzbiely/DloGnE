ca2faab933e338d3d906b4176a49da1ab3baac0a
Francis Albores <mrthemetribe@gmail.com>
2017-10-02 18:52:45 +0800

    Showing archive lists

--- theprofpgsvis/app/js/controllers/PropertiesController.js
+++ theprofpgsvis/app/js/controllers/PropertiesController.js
@@ -1,23 +1,30 @@
 angular.module('MetronicApp').controller('PropertiesController', 
-    function($rootScope, $scope, $http, settings) {
+    function($rootScope, $scope, $http, settings, $state) {
+        $scope.archives = [];
+        $scope.properties = [];
+        $scope.error;
+        $scope.property;
+        $scope.isDisabled = false;
         $scope.$on('$viewContentLoaded', function() {   
             App.initAjax();
-            $scope.properties = [];
-            $scope.error;
-            $scope.property;
-            $scope.isDisabled = false;
         });
         $scope.hasActions = $scope.$parent.type !== "reports" ? true : false;
-        // Display
+
+        
+
         $scope.init = function() {
-            $http.get($rootScope.apiURL + 'v1/property?token='+localStorage.getItem('satellizer_token')).success(function(property) {
+            var url = $rootScope.apiURL + 'v1/property?token='+localStorage.getItem('satellizer_token');
+            if($state.current.name === "property.archives") {
+                url = $rootScope.apiURL + 'v1/property/param/is_archive=1?token='+localStorage.getItem('satellizer_token');
+            }
+            $http.get(url).success(function(property) {
                 $scope.properties = property.data;
             }).error(function(error) {
                 console.log('Service error : ',error);
                 if(error.error == "token_expired")
                     $rootScope.logout();
             })
-        };
+        }
         $scope.init();
 
         // Delete
@@ -30,5 +37,17 @@ angular.module('MetronicApp').controller('PropertiesController',
                 });;
         }
 
+        // Archive
+        $scope.archiveProperty = function(index, id) {
+            $scope.isDisabled = true;
+            var param = {
+                is_archive : 1
+            };
+            $http.patch($rootScope.apiURL + 'v1/property/' + id + '?token='+localStorage.getItem('satellizer_token'), param)
+                .success(function() {
+                    $scope.properties.splice(index, 1);
+                    $scope.isDisabled = false;
+                });;
+        }
     }
 );
