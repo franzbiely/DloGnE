6f7cbc287b2e92f9affb3e5cae6090b44834416c
Francis Albores <mrthemetribe@gmail.com>
2017-12-04 00:56:59 +0800

    Fix PDF filename bug on remaining (_) underscore

--- theprofpgsvis/app/js/filters.js
+++ theprofpgsvis/app/js/filters.js
@@ -1,6 +1,6 @@
 MetronicApp.filter('pretty_filename', function() {
     return function(input) {
         input = input || '';
-        return input.substring(input.indexOf('__') + 2,input.length);
+        return input.substring(input.indexOf('___') + 3,input.length);
     };
 });
