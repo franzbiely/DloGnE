65300ee814a5594ae46143063dfe7dfb2e1f89ad
Francis Albores <mrthemetribe@gmail.com>
2018-01-09 16:49:42 +0800

    API : Model Rent update

--- theprofpgapi/app/Rent.php
+++ theprofpgapi/app/Rent.php
@@ -3,8 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Property;
 
 class Rent extends Model
 {
-    //
+    protected $fillable = [
+		'property_id',
+        'analysed_rent',
+        'analysed_date',
+        'remarks'
+	];
+	protected $hidden = ['created_at', 'updated_at'];
+	
+	public function property(){
+        return $this->belongsTo('App\Property');
+    }
 }
