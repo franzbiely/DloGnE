f6fb90d16b5f7040d266e4ccfc31c02f5c181e39
Francis Albores <mrthemetribe@gmail.com>
2018-06-06 09:34:45 +0800

    Configure sales detail controller for the new sales detail page

--- theprofpgsvis/app/js/controllers/SalesDetailsController.js
+++ theprofpgsvis/app/js/controllers/SalesDetailsController.js
@@ -6,19 +6,42 @@ angular.module('MetronicApp').controller('SalesDetailsController',
         $scope.photos = [];  
         $scope.data.photo_ids = [];
         $scope.data.pdf_ids = []; 
+        
         // Load Data for Edit
         $scope.params = $stateParams; 
-
         $scope.isDisabled = false;
+        $rootScope.pageSidebarClosed = false;
+        $('.date-picker').datepicker({
+            format: 'dd-mm-yyyy',
+            autoclose: true}).on('changeDate', function (ev) {
+               $(this).blur();
+               $(this).datepicker('hide');
+        });
+        var isEdit = ($scope.params.sales_id !== "" && typeof $scope.params.sales_id !== 'undefined') ? true : false;
+        if(isEdit) {
+            $scope.page_title = "Edit Sales";
+            loadData($scope.params.sales_id);
+        }    
+        else {
+            $scope.page_title = "New Sales";
+            $scope.data.property_id = $state.params.property_id;
+        }
 
         function loadData(id) {
             $http.get($rootScope.apiURL + 'v1/sale/'+ id +'?token='+localStorage.getItem('satellizer_token')).success(function(response) {
-                $scope.data.id = response.id;
+                $scope.data.id = response.data.id;
                 $scope.data.date = moment(response.data.date, 'YYYY-MM-DD').format('DD-MM-YYYY');
-                $scope.data.purchaser = response.data.purchaser;
-                $scope.data.vendor = response.data.vendor;
-                $scope.data.value = response.data.value;
                 $scope.data.description = response.data.description;
+                $scope.data.est_improvement_value = response.data.est_improvement_value;
+                $scope.data.est_land_rate = response.data.est_land_rate;
+                $scope.data.est_land_value = response.data.est_land_value;
+                $scope.data.price = response.data.price;
+                $scope.data.property = response.data.property;
+                $scope.data.property_id = response.data.property_id;
+                $scope.data.purchaser = response.data.purchaser;
+                $scope.data.remarks = response.data.remarks;
+                $scope.data.source = response.data.source;
+                $scope.data.vendor = response.data.vendor;
             }).error(function(error){
                 if(!FUNC.tryLogout(error)) {
                     console.log(error);  
@@ -54,136 +77,53 @@ angular.module('MetronicApp').controller('SalesDetailsController',
 
         $scope.$on('$viewContentLoaded', function() {
             App.initAjax();
-            // Dropzone.autoDiscover = false;
-            loadData($scope.params.sales_id);
+            Dropzone.autoDiscover = false;
         });
-
-        // $scope.dzOptions = {
-        //     url : '/alt_upload_url',
-        //     paramName : 'photo',
-        //     maxFilesize : '10',
-        //     acceptedFiles : 'image/jpeg, images/jpg, image/png',
-        //     addRemoveLinks : true
-        // };
-        // $scope.dzCallbacks = {
-        //     'addedfile' : function(file){
-        //         console.log(file);
-        //         $scope.newFile = file;
-        //     },
-        //     'success' : function(file, xhr){
-        //         console.log(file, xhr);
-        //     }
-        // };
-        // $scope.dzMethods = {};
-        // var uploader = $scope.uploader = new FileUploader({
-        //     url: '../assets/global/plugins/angularjs/plugins/angular-file-upload/upload.php'
-        // });
-        // // FILTERS
-        // uploader.filters.push({
-        //     name: 'customFilter',
-        //     fn: function(item /*{File|FileLikeObject}*/ , options) {
-        //         return this.queue.length < 10;
-        //     }
-        // });
-        // $scope.removeNewFile = function(){
-        //     $scope.dzMethods.removeFile($scope.newFile); //We got $scope.newFile from 'addedfile' event callback
-        // }
-        
-        // // CALLBACKS
-        // uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/ , filter, options) {
-        //     console.info('onWhenAddingFileFailed', item, filter, options);
-        // };
-        // uploader.onAfterAddingFile = function(fileItem) {
-        //     console.info('onAfterAddingFile', fileItem);
-        // };
-        // uploader.onAfterAddingAll = function(addedFileItems) {
-        //     console.info('onAfterAddingAll', addedFileItems);
-        // };
-        // uploader.onBeforeUploadItem = function(item) {
-        //     console.info('onBeforeUploadItem', item);
-        // };
-        // uploader.onProgressItem = function(fileItem, progress) {
-        //     console.info('onProgressItem', fileItem, progress);
-        // };
-        // uploader.onProgressAll = function(progress) {
-        //     console.info('onProgressAll', progress);
-        // };
-        // uploader.onSuccessItem = function(fileItem, response, status, headers) {
-        //     console.info('onSuccessItem', fileItem, response, status, headers);
-        // };
-        // uploader.onErrorItem = function(fileItem, response, status, headers) {
-        //     console.info('onErrorItem', fileItem, response, status, headers);
-        // };
-        // uploader.onCancelItem = function(fileItem, response, status, headers) {
-        //     console.info('onCancelItem', fileItem, response, status, headers);
-        // };
-        // uploader.onCompleteItem = function(fileItem, response, status, headers) {
-        //     console.info('onCompleteItem', fileItem, response, status, headers);
-        // };
-        // uploader.onCompleteAll = function() {
-        //     console.info('onCompleteAll');
-        // };
-        
-        // $scope.select_change = function(res, model) {
-        //     $scope[model] = res;
-        // }
-        
-        // console.log($scope.params);
         // Add
         $scope.save = function() {
-
             $scope.isDisabled = true;
             var param = {
                 date : $scope.data.date,
-                purchaser : $scope.data.purchaser,
-                vendor : $scope.data.vendor,
-                value : $scope.data.value,
                 description : $scope.data.description,
-                photo_ids : $scope.data.photo_ids,
+                est_improvement_value : $scope.data.est_improvement_value,
+                est_land_rate : $scope.data.est_land_rate,
+                est_land_value : $scope.data.est_land_value,
+                price : $scope.data.price,
+                property : $scope.data.property,
+                property_id : $scope.data.property_id,
+                purchaser : $scope.data.purchaser,
+                remarks : $scope.data.remarks,
+                source : $scope.data.source,
+                vendor : $scope.data.vendor,
                 pdf_ids : $scope.data.pdf_ids
             };
 
-            if($scope.params.sales_id !== "") {
-                // if edit
-                $http.put($rootScope.apiURL + 'v1/sale/' + $scope.params.sales_id + '?token='+localStorage.getItem('satellizer_token'), param).success(function(response) {
-                    alert('Update Successfully');
-                    $state.go('property.sales', {property_id : $scope.params.property_id});    
-                }).error(function(){
-                    console.log("error");
+            if($state.current.name === 'sales.new') {
+                // if add
+                $http.post($rootScope.apiURL + 'v1/sale?token='+localStorage.getItem('satellizer_token'), param).success(function(response) {
+                    $state.go('sales.list', {property_id : $scope.data.property_id});    
+
+                    alert('Added Successfully');
+                }).error(function(error){
+                    if(!FUNC.tryLogout(error)) {
+                        console.log(error);  
+                    }
                 });
             }
             else {
-                // if add
-                $http.post($rootScope.apiURL + 'v1/sale?token='+localStorage.getItem('satellizer_token'), param).success(function(response) {
-                    $state.go('property.sales', {property_id : $scope.params.property_id});    
-                }).error(function(){
-                    console.log("error");
+                
+                // // if edit
+                $http.put($rootScope.apiURL + 'v1/sale/' + $scope.params.sales_id + '?token='+localStorage.getItem('satellizer_token'), param).success(function(response) {
+                    alert('Update Successfully');
+                    $state.go('sales.list', {property_id : $scope.data.property_id});    
+                }).error(function(error){
+                    if(!FUNC.tryLogout(error)) {
+                        console.log(error);  
+                    }
                 });
-            }   
+            }  
         }
 
-        //======= dropzone for photos===========
-        $scope.dzOptions = {
-            url : $rootScope.apiURL + 'v1/media' + '?token='+localStorage.getItem('satellizer_token'),
-            paramName : 'photo',
-            params : {
-                source_table : 'sales',
-                media_type : 'image',
-                source_id : 0
-            },
-            maxFilesize : '0.25',
-            acceptedFiles : 'image/jpeg, images/jpg, image/png',
-            addRemoveLinks : true
-        };
-        $scope.dzCallbacks = {
-            'addedfile' : function(file){
-                $scope.newFile = file;
-            },
-            'success' : function(file, xhr){
-                $scope.data.photo_ids.push(xhr.data.id);
-            }
-        };
-        //========= /dropzone ========
         //======= dropzone for PDF===========
         $scope.dzOptionsPDF = {
             url : $rootScope.apiURL + 'v1/media' + '?token='+localStorage.getItem('satellizer_token'),
@@ -201,17 +141,18 @@ angular.module('MetronicApp').controller('SalesDetailsController',
             'addedfile' : function(file){
                 $scope.newPDFFile = file;
             },
+            'maxfilesexceeded' : function(file) {
+                alert("No more files please!");
+            },
             'success' : function(file, xhr){
                 $scope.data.pdf_ids.push(xhr.data.id);
+            },
+            'addedfile' : function(file) {
+                $(file.previewElement).find(".dz-image img").attr("src", "assets/img/pdf.png");
             }
         };
         //========= /dropzone PDF ========
-        $scope.removePhoto = function(key) {
-            delete $scope.data.photo_ids[key];
-            delete $scope.photos[key];
-            $scope.photos.splice(key, 1);
-            $scope.data.photo_ids.splice(key,1);
-        }
+
         $scope.removePDF = function(key) {
             delete $scope.data.pdf_ids[key];
             delete $scope.pdfs[key];
